792f5a428f87463c074a93e0cb863eabf237dabc
John Popplewell <jpopplewell+git@gmail.com>
2017-02-10 10:42:29 -0600

    MVP skeleton

---
+++ app/src/main/java/edu/utdallas/csdesign/spring17/nutriscope/BasePresenter.java
@@ -0,0 +1,10 @@
+package edu.utdallas.csdesign.spring17.nutriscope;
+
+/**
+ * Created by john on 2/10/17.
+ */
+
+public interface BasePresenter {
+    void start();
+
+}
---
+++ app/src/main/java/edu/utdallas/csdesign/spring17/nutriscope/Meals/MealsActivity.java
@@ -0,0 +1,10 @@
+package edu.utdallas.csdesign.spring17.nutriscope.Meals;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by john on 2/10/17.
+ */
+
+public class MealsActivity extends AppCompatActivity {
+}
