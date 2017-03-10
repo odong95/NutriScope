3a7f3163ed5162ec00a5b2130691b35366b89ef5
John Popplewell <jpopplewell+git@gmail.com>
2017-03-10 14:00:08 -0600

    add quickstart email login activity

---
+++ app/src/main/java/edu/utdallas/csdesign/spring17/nutriscope/data/source/firebase/FirebaseClient.java
@@ -0,0 +1,8 @@
+package edu.utdallas.csdesign.spring17.nutriscope.data.source.firebase;
+
+/**
+ * Created by john on 3/10/17.
+ */
+
+public class FirebaseClient {
+}
---
+++ app/src/main/java/edu/utdallas/csdesign/spring17/nutriscope/login/LoginActivity.java
@@ -0,0 +1,8 @@
+package edu.utdallas.csdesign.spring17.nutriscope.login;
+
+/**
+ * Created by john on 3/10/17.
+ */
+
+public class LoginActivity {
+}
